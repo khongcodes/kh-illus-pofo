95716853683815102c6360e1629197a5cfcb55f2
KHong <albtrossfacefall@gmail.com>
2020-06-04 12:25:10 -0400

    create toggling hamburger

--- src/viewsCommon/Layout.tsx
+++ src/viewsCommon/Layout.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 // import menuData from '../configData/menuData.json';
 import { muddyMenuData, LinkShape, SubmenuShape } from '../model/MenuShape'
+
+import siteLogo from '../images/site-logo.png'
 
 import layoutStyles from '../style/Layout.module.sass';
 
@@ -10,11 +12,28 @@ import layoutStyles from '../style/Layout.module.sass';
 type LayoutProps = {
   children: React.ReactNode[] | React.ReactNode
 }
-type MenuProps = {
+type MenuDataProps = {
   data: (LinkShape|SubmenuShape)[]
 }
+type MobileMenuControlProps = {
+  mobileMenuOpen: boolean;
+  handleMobileMenuToggle?: () => void;
+  resetMobileMenu: () => void;
+}
 
-const TogglingSubmenu = ({ item }: { item: SubmenuShape }) => {
+const ListItemNavLink = ({ item, resetMobileMenu }: {
+  item: LinkShape;
+  resetMobileMenu: () => void;
+}) => (
+  <li key={item.path}>
+    <NavLink to={item.path} onClick={resetMobileMenu}> {item.text} </NavLink>
+  </li>
+)
+
+const TogglingSubmenu = ({ item, resetMobileMenu }: { 
+  item: SubmenuShape;
+  resetMobileMenu: () => void;
+ }) => {
   const [submenuOpen, toggleSubmenu] = useState(false);
   const handleSubmenuToggle = () => toggleSubmenu(!submenuOpen)
 
@@ -32,10 +51,8 @@ const TogglingSubmenu = ({ item }: { item: SubmenuShape }) => {
           item.items.map(subitem => {
             if (subitem.type==='link') {
               return (
-                <li key={subitem.path}>
-                  <NavLink to={subitem.path}> {subitem.text} </NavLink>
-                </li>
-              )   
+                <ListItemNavLink item={subitem} resetMobileMenu={resetMobileMenu}/>
+              )
             }
           }
         )}
@@ -44,16 +61,17 @@ const TogglingSubmenu = ({ item }: { item: SubmenuShape }) => {
   )
 }
 
-const LockedSubmenu = ({ item }: { item: SubmenuShape }) => (
+const LockedSubmenu = ({ item, resetMobileMenu }: {
+  item: SubmenuShape;
+  resetMobileMenu: () => void;
+}) => (
   <li>
     {item.text}
     <ul>
       {item.items.map(subitem => {
         if (subitem.type==='link') {
           return (
-            <li key={subitem.path}>
-              <NavLink to={subitem.path}> {subitem.text} </NavLink>
-            </li>
+            <ListItemNavLink item={subitem} resetMobileMenu={resetMobileMenu}/>
           )
         }
       })}
@@ -63,64 +81,110 @@ const LockedSubmenu = ({ item }: { item: SubmenuShape }) => (
 
 const mapMenuContent = (
   data: (LinkShape|SubmenuShape)[],
-  submenuType: "toggling"|"locked"
+  submenuType: "toggling"|"locked",
+  resetMobileMenu: () => void
   ) => (
   data.map(item => {
+    const linkTriggersMobileMenu = !!resetMobileMenu;
+
     switch(item.type) {
 
       case "link":
         return (
-          <li key={item.path}>
-            <NavLink to={item.path}> {item.text} </NavLink>
-          </li>
-        );
+          <ListItemNavLink item={item} resetMobileMenu={resetMobileMenu}/>
+        )
 
       case "submenu":
         switch (submenuType) {
           case "toggling":
             return (
-              <TogglingSubmenu item={item} key={item.text}/>
+              <TogglingSubmenu item={item} key={item.text} resetMobileMenu={resetMobileMenu}/>
             )
           case "locked":
             return (
-              <LockedSubmenu item={item} key={item.text}/>
+              <LockedSubmenu item={item} key={item.text} resetMobileMenu={resetMobileMenu}/>
             )
         }
     }
   })
 )
 
-const SideMenu = ({ data }: MenuProps): JSX.Element => {
+const MobileMenuControl = ({ mobileMenuOpen, handleMobileMenuToggle }: MobileMenuControlProps) => {
+  return (
+    <div 
+      className={`${layoutStyles.mobileMenuControlContainer} ${mobileMenuOpen ? layoutStyles.open : ''}`}
+      onClick={handleMobileMenuToggle}
+    >
+      <div className={layoutStyles.bar1}/>
+      <div className={layoutStyles.bar2}/>
+      <div className={layoutStyles.bar3}/>
+    </div>
+  )
+}
+
+const SideMenu = ({ data, mobileMenuOpen, handleMobileMenuToggle, resetMobileMenu }: MenuDataProps & MobileMenuControlProps): JSX.Element => {
+  
   return (
     <div className={layoutStyles.sideMenuContainer} >
+      {/* Site Logo */}
+      <Link to='/'>
+        <img 
+          src={siteLogo}
+          id={layoutStyles.siteLogo}
+        />
+      </Link>
+      
+      {/* Side menu, display:none on mobile-media */}
       <ul className={layoutStyles.sideMenuList}>
-        {mapMenuContent(data, "toggling")}
+        {mapMenuContent(data, "toggling", resetMobileMenu)}
       </ul>
+
+      {/* Toggle mobile menu, display:none on desktop-media */}
+      <MobileMenuControl 
+        mobileMenuOpen={mobileMenuOpen}
+        handleMobileMenuToggle={handleMobileMenuToggle}
+        resetMobileMenu={resetMobileMenu}
+      />
     </div>
   )
 }
 
-const MobileMenuDrawer = ({ data }: MenuProps): JSX.Element => {
+const MobileMenuDrawer = ({ data, mobileMenuOpen, resetMobileMenu }: MenuDataProps & MobileMenuControlProps): JSX.Element => {
   return (
-    <div className={layoutStyles.mobileMenuContainer} >
+    <div className={`${layoutStyles.mobileMenuContainer} ${mobileMenuOpen ? layoutStyles.open : ''}`} >
       <ul>
-        {mapMenuContent(data, "locked")}
+        {mapMenuContent(data, "locked", resetMobileMenu)}
       </ul>
     </div>
   )
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <>
-    <MobileMenuDrawer data={muddyMenuData} />
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [mobileMenuOpen, toggleMobileMenu] = useState(false);
+  const handleMobileMenuToggle = () => toggleMobileMenu(!mobileMenuOpen)
+  const resetMobileMenu = () => toggleMobileMenu(false)
 
-    <div id={layoutStyles.appLayout} >
-      <SideMenu data={muddyMenuData} />
-      <div id={layoutStyles.pageContentContainer}>
-        {children}
+  return (
+    <>
+      <MobileMenuDrawer 
+        data = {muddyMenuData} 
+        mobileMenuOpen = {mobileMenuOpen}
+        resetMobileMenu = {resetMobileMenu}
+      />
+
+      <div id={layoutStyles.appLayout} >
+        <SideMenu 
+          data = {muddyMenuData} 
+          mobileMenuOpen = {mobileMenuOpen}
+          handleMobileMenuToggle = {handleMobileMenuToggle}
+          resetMobileMenu = {resetMobileMenu}
+        />
+        <div id={layoutStyles.pageContentContainer}>
+          {children}
+        </div>
       </div>
-    </div>
-  </>
-)
+    </>
+  )
+}
 
 export default Layout
